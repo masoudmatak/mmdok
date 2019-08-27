3ca0f052da1617fb012fc6cbfffc523cb1f4b5bf
emagnca <emagnca@yahoo.se>
2019-08-27 23:07:22 +0200

    .

--- server/csv2json.js
+++ server/csv2json.js
@@ -1,25 +1,31 @@
-const csvFilePath='config/permissions/permissions.csv'
-const csv=require('csvtojson')
-var json = "";
-csv({delimiter:':'})
- .fromFile(csvFilePath)
- .then((json)=>{
-	 json.forEach(o => {
-		 let source = o['Source'];
-		 let operation = o['Operation'];
-		 let groups = JSON.stringify(o['Groups'].split(','));
-		 json = getRule(source, operation, groups);
-		 if (json == "") { }
-	     });
-     });
 
-function getRule(source, operation, groups){
-    return '{ "and": [{ "==": [{ "var": "source" }, "' + source + 
-	'"]}, {"match": [ {"var": "groups"}, ' + groups + '] }, {"==": [{"var": "operation"}, "' + operation + '"]}] }';
-	
+class Csv2Json {
+	getRules() {
+		const csvFilePath = 'config/permissions/permissions.csv'
+		const csv = require('csvtojson')
+
+
+		csv({ delimiter: ':' })
+			.fromFile(csvFilePath)
+			.then((json) => {
+				var s = '{"or": [';
+				json.forEach((o, i) => {
+					if (i > 0) s += ',';
+					let source = o['Source'];
+					let operation = o['Operation'];
+					let groups = JSON.stringify(o['Groups'].split(','));
+					s += this.getRule(source, operation, groups);
+				})
+				s += ']}';
+				console.log(s);
+			});
+
+	}
+
+	getRule(source, operation, groups) {
+		return '{ "and": [{ "==": [{ "var": "source" }, "' + source +
+			'"]}, {"match": [ {"var": "groups"}, ' + groups + '] }, {"==": [{"var": "operation"}, "' + operation + '"]}] }';
+
+	}
 }
-
-/*
-{ "and": [{ "==": [{ "var": "source" }, "G90"]}, {"match": [ {"var": "groups"}, ["Kundservice", "KOReadGroup"]] }, {"==": [{"var": "operation"}, "read"]}] }
-{ "and": [{ "==": [{ "var": "source" }, "G90"]}, {"match": [ {"var": "groups"}, ["G90ReadGroup","Kundservice"]] }, {"==": [{"var": "operation"}, "read"]}] }
-*/
+new Csv2Json().getRules();
