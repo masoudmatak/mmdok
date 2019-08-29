cf7c8bcc3c9d56e8cf6e82f82f275c35f0577672
emagnca <emagnca@yahoo.se>
2019-08-29 22:48:09 +0200

    .

--- server/app.js
+++ server/app.js
@@ -1,40 +1,101 @@
-var Express = require("express");
-var Multer = require("multer");
-var Minio = require("minio");
-var app = Express();
+const Express = require("express");
+const Multer = require("multer");
+const Minio = require("minio");
+const MongoClient = require('mongodb').MongoClient;
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
+
+const uuid = require('uuid/v1');
+const url = "mongodb://localhost:27017/";
+const COLLECTION = "metadata";
+
+const app = Express();
 
 var minioClient = new Minio.Client({
-    endPoint: 'localhost',
-    port: 9000,
-    useSSL: false,
-    accessKey: '5KJ9URBASB7N0PR4L2P6',
-    secretKey: 'm9emLlQYLHatBTOiK9MBqhz4iitPrxb0QKrqMoia'
+	endPoint: 'localhost',
+	port: 9000,
+	useSSL: false,
+	accessKey: '5KJ9URBASB7N0PR4L2P6',
+	secretKey: 'm9emLlQYLHatBTOiK9MBqhz4iitPrxb0QKrqMoia'
 });
 
 app.post("/upload", Multer({ storage: Multer.memoryStorage() }).single("upload"), function (request, response) {
-    minioClient.putObject(request.body.bucket, request.file.originalname, request.file.buffer, function (error, etag) {
-        if (error) {
-            return console.log(error);
-        }
-        response.send('File uploaded');
-    });
+	minioClient.putObject(request.body.bucket, request.file.originalname, request.file.buffer, function (error, etag) {
+		if (error) {
+			return console.log(error);
+		}
+		let metadata = JSON.parse(request.body.metadata);
+		metadata['bucket'] = request.body.bucket;
+		metadata['name'] = request.file.originalname;
+		metadata['_id'] = uuid();
+		console.log(metadata);
+		insertMetadata(metadata);
+		response.send(metadata['_id']);
+	});
 });
 
 app.get("/download", function (request, response) {
-    minioClient.getObject(request.query.bucket, request.query.filename, function (error, stream) {
-        if (error) {
-            return response.status(500).send(error);
-        }
-        let suffix = request.query.filename.split('.').pop();
-        if (suffix === 'pdf'){
-               response.setHeader('Content-disposition', 'inline; filename="' + request.query.filename + '"');
-               response.setHeader('Content-type', 'application/pdf');
-        }
-        stream.pipe(response);
-    });
-   
+	var error;
+	getMetadata(request.query.id, function (error, metadata) {
+		if (error) {
+			console.log(err);
+			response.send("No such id");
+		}
+
+		if (metadata) {
+			console.log('account exists');
+			console.dir(metadata);
+			minioClient.getObject(metadata.bucket, metadata.name, function (error, stream) {
+				if (error) {
+					return response.status(500).send(error);
+				}
+				let suffix = metadata.name.split('.').pop();
+				if (suffix === 'pdf') {
+					response.setHeader('Content-disposition', 'inline; filename="' + metadata.name + '"');
+					response.setHeader('Content-type', 'application/pdf');
+				}
+				stream.pipe(response);
+			});
+
+		} else {
+			console.log('good to go');
+			response.send("No metadata");
+		}
+
+	});
+
 });
 
+function insertMetadata(metadata) {
+	MongoClient.connect(url, function (err, db) {
+		if (err) throw err;
+		var dbo = db.db("mmdok");
+		dbo.collection("metadata").insertOne(metadata, function (err, res) {
+			if (err) throw err;
+			console.log("1 document inserted");
+			db.close();
+		});
+	});
+}
+
+var getMetadata = function (id, callback) {
+	MongoClient.connect(url, function (err, db) {
+		let query = { _id: id };
+		console.log(query);
+		if (err) throw err;
+		var dbo = db.db("mmdok");
+		dbo.collection('metadata').findOne(query, function (err, doc) {
+			db.close();
+			if (err) {
+				callback(err);
+			}
+			else {
+				callback(null, doc);
+			}
+		});
+	});
+}
+
 var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
+	console.log("Listening on port %s...", server.address().port);
 });
