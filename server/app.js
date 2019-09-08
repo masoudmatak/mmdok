ea0c9e2e2b3ec77d8220f93dbf9f1ad049b99067
emagnca <emagnca@yahoo.se>
2019-09-08 22:49:48 +0200

    .

--- server/app.js
+++ server/app.js
@@ -1,54 +1,50 @@
 'use strict';
+
 const Express = require("express");
 const Multer = require("multer");
-const Minio = require("minio");
+const UUID = require('uuid/v1');
 const MongoConnector = require("./database");
 const S3 = require("./s3client");
-const uuid = require('uuid/v1');
-const url = "mongodb://localhost:27017/";
-const COLLECTION = "metadata";
+
 const s3 = new S3();
 const db = new MongoConnector();
 const app = Express();
 
 app.post("/document", Multer({ storage: Multer.memoryStorage() }).single("upload"), function (request, response) {
-	let id = uuid();
-	db.connect()
-		.then(() => { db.put({ _id: uuid(), name: request.file.originalname, bucket: request.body.bucket }); })
-		.then(() => { s3.put(request.body.bucket, request.file.originalname, request.file.buffer); })
-		.then(() => { response.send(id); })
-		.catch((error) => { console.log(error); response.status(500).send(error); });
-	//minioClient.putObject(request.body.bucket, request.file.originalname, request.file.buffer)
-	/*.then(() => MongoClient.connect(url)
-.then(client => client.db("mmdok").collection("metadata"))
-.then(c => { c.insertOne({ _id: uuid(), name: request.file.originalname, bucket: request.body.bucket }); console.log(_id); return _id; })
-.then(id => { console.log("HEJ!!!!!!!!!!"); console.log(id); response.send(id); } )
-.catch( error => response.status(500).send(error) ) */
+	let id = UUID();
+	let metadata = Object.assign({ _id: id, name: request.file.originalname, bucket: request.body.bucket }, JSON.parse(request.body.metadata));
+	db.put(metadata)
+          .then(() => { s3.put(request.body.bucket, request.file.originalname, JSON.parse(request.file.buffer)); })
+          .then(() => { response.send(id); })
+          .catch((error) => { rollback(id); response.status(500).send(error.message); });
 });
 
-app.get("/document"), function (request, response) {
-	MongoClient.connect(url)
-		.then(client => client.db("mmdok").collection("metadata"))
-		.then(c => c.findOne({ _id: request.query.id }))
-		.then((metadata) => {
-			minioClient.getObject(metadata.bucket, metadata.name, function (error, stream) {
-				if (error) {
-					return response.status(500).send(error);
-				}
-				let suffix = metadata.name.split('.').pop();
-				if (suffix === 'pdf') {
-					response.setHeader('Content-disposition', 'inline; filename="' + metadata.name + '"');
-					response.setHeader('Content-type', 'application/pdf');
-				}
-				stream.pipe(response);
-			});
-		});
+app.get("/document", function (request, response) {
+	validateRequest(request);
+	db.get({ _id: request.query.id })
+		.then( metadata => setHeaders(response, metadata)) 
+		.then( metadata => s3.get(metadata.bucket, metadata.name))
+		.then( stream => stream.pipe(response))
+		.catch( error => { response.status(500).send(error.message); }); 
+});
+
+function rollback(id) {
+	// TODO
 }
 
+function validateRequest(request) {
+	// TODO
+}
 
+function setHeaders(response, metadata){
+	return new Promise((resolve) => { 
+		response.setHeader('Content-disposition', 'inline; filename="' + metadata.name + '"');
+		response.setHeader('Content-type', 'application/pdf');
+		response.setHeader('metadata', metadata);
+		resolve(metadata);
+	});
+}
 
 var server = app.listen(3000, function () {
 	console.log("Listening on port %s...", server.address().port);
 });
-
-console.log("Done");
